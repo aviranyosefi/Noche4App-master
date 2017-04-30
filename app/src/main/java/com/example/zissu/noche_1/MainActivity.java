67bbc5c835f6b7e019eb55d0e423a2a3398b1e2c
aviranyosefi <aviran5600@gmail.com>
2017-04-30 14:36:13 +0300

    with login

--- app/src/main/java/com/example/zissu/noche_1/MainActivity.java
+++ app/src/main/java/com/example/zissu/noche_1/MainActivity.java
@@ -1,29 +1,47 @@
 package com.example.zissu.noche_1;
 
-
+import android.graphics.Color;
 import android.os.Bundle;
-
-
-import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v7.view.menu.MenuView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.SSLContext;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener ,  View.OnClickListener {
 
-    private Button mSendData;
+    private Button mSendData , b_login , b_register;
     private SearchFragment fragment = null;
     private android.app.FragmentManager fragmentManager;
+
+
 
 
     @Override
@@ -32,6 +50,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        b_login = (Button) findViewById(R.id.bLogin);
+        b_login.setOnClickListener((View.OnClickListener) this);
+        b_register = (Button) findViewById(R.id.bRegister);
+        b_register.setOnClickListener((View.OnClickListener) this);
+
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -44,6 +68,29 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         displayView(0);
     }
+
+
+    @Override
+    public void onClick(View v) {
+       if ( v == b_login ) {
+
+           Login login_fragment = new Login();
+           FragmentManager manager = getSupportFragmentManager();
+           manager.beginTransaction().replace(R.id.content_main_layout,
+                   login_fragment, login_fragment.getTag()).commit();
+       }
+
+           else if ( v == b_register) {
+           Register register_fragment = new Register();
+           FragmentManager manager1 = getSupportFragmentManager();
+           manager1.beginTransaction().replace(R.id.content_main_layout,
+                   register_fragment, register_fragment.getTag()).commit();
+
+       }
+
+
+       }
+
 
     @Override
     public void onBackPressed() {
@@ -58,7 +105,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-       // getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -110,6 +157,49 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
+    public void onClickBar (View view){
+        BarsFragment barsFragment = new BarsFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content_main_layout ,
+                barsFragment ,barsFragment.getTag() ).commit();
+        try {
+            URL url = new URL("http://193.106.55.242:8080/getAllPlaces");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+             response.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void onClickClub (View view){
+        ClubsFragment clubsFragment = new ClubsFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content_main_layout ,
+                clubsFragment ,clubsFragment.getTag() ).commit();
+
+    }
+    public void onClickRes (View view){
+        RestaurantsFragment restaurantsFragment = new RestaurantsFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content_main_layout ,
+                restaurantsFragment ,restaurantsFragment.getTag() ).commit();
+
+    }
+
+
+
+
+    //serach bar funcation
     private void displayView(int position) {
         fragment = null;
         String fragmentTags = "";
@@ -128,25 +218,4 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public void onClickBar (View view){
-        BarsFragment barsFragment = new BarsFragment();
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.content_main_layout ,
-                barsFragment ,barsFragment.getTag() ).commit();
-
-    }
-    public void onClickClub (View view){
-        ClubsFragment clubsFragment = new ClubsFragment();
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.content_main_layout ,
-                clubsFragment ,clubsFragment.getTag() ).commit();
-
-    }
-    public void onClickRes (View view){
-        RestaurantsFragment restaurantsFragment = new RestaurantsFragment();
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.content_main_layout ,
-                restaurantsFragment ,restaurantsFragment.getTag() ).commit();
-
-    }
 }
